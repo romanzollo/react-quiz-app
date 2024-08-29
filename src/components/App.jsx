14cd5f26f967948a44f516c0b2920dd8b261107b
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-08-29 20:57:38 +0200

    fetch for json-server-vercel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -128,7 +128,7 @@ function App() {
     );
 
     useEffect(() => {
-        fetch('http://localhost:8000/questions')
+        fetch('https://json-server-vercel-six-roan.vercel.app/questions')
             .then((res) => res.json())
             .then((data) => dispatch({ type: 'dataReceived', payload: data }))
             .catch((err) => dispatch({ type: 'dataFailed' }));
